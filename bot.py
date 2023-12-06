d17d78b55bf38538ccbb3811a16df7a754b87dd9
stevejj9403 <stevejung394@gmail.com>
2023-12-05 18:33:01 -0600

    integrated chatpgt

--- bot.py
+++ bot.py
@@ -1,11 +1,38 @@
-import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+from openai import OpenAI
+import pandas as pd
+import time
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-client = slack.WebClient(token = os.environ['SLACK_TOKEN'])
+app = App(token = os.environ["SLACK_TOKEN_USER"])
 
-client.chat_postMessage(channel='#gid-help-nursery', text = "Hello")
+chatClient = OpenAI(api_key = os.environ['OPENAI_TOKEN'])
+
+@app.event("app_mention")    
+def handle_message_events(body, say):
+    # Log message
+    print(str(body["event"]["text"]).split(">")[1])
+    
+    # Create prompt for ChatGPT
+    prompt = str(body["event"]["text"]).split(">")[1]
+    
+    say("Getting your message...")
+
+    completion = chatClient.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": prompt},
+        {"role": "user", "content": "Hello!"}
+        ]
+    )
+    say(completion.choices[0].message.content)
+
+if __name__ == "__main__":
+    SocketModeHandler(app, os.environ["SLACK_TOKEN_APP"]).start()
